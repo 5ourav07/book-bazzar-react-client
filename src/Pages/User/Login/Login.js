21845910ab23f7e2919e9e5d4b615a43f37785f8
Sourav Saha <shanto.sourav07@gmail.com>
2022-11-26 01:49:24 +0600

    Firebase & Hook Form Done

--- src/Pages/User/Login/Login.js
+++ src/Pages/User/Login/Login.js
@@ -1,44 +1,55 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [loginError, setLoginError] = useState('');
+
+    const handleLogin = data => {
+        console.log(data);
+    }
+
     return (
         <Fragment>
-            <div className="hero flex h-screen justify-center items-center bg-black">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="card flex-shrink-0 shadow-2xl bg-white">
-                        <h1 className="text-center text-black text-3xl font-bold mt-3">Login</h1>
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
+            <div className='pt-20 flex justify-center items-center bg-black'>
+                <div className='border border-white mb-8 p-16'>
+                    <h2 className='text-5xl text-white mb-8 text-center'>Login</h2>
 
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label mt-2">
-                                    <Link to='/signup' className="label-text-alt link link-hover">
-                                        Don't Have an Account? Sign Up Here
-                                    </Link>
-                                </label>
-                            </div>
-
-                            <div className="form-control">
-                                <button className="btn btn-primary">Login</button>
-                            </div>
-
-                            <div className="divider text-black">OR</div>
-
-                            <div className="form-control">
-                                <button className="btn btn-danger">Google</button>
-                            </div>
+                    <form onSubmit={handleSubmit(handleLogin)}>
+                        <div className="form-control w-full">
+                            <input placeholder='Email' type="email"
+                                {...register("email", {
+                                    required: "Your Email Address is Required"
+                                })}
+                                className="input input-bordered w-full my-3" />
+                            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
                         </div>
-                    </div>
+
+                        <div className="form-control w-full">
+                            <input placeholder='******' type="password"
+                                {...register("password", {
+                                    required: "Your Password is Required",
+                                    minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                                })}
+                                className="input input-bordered w-full my-3" />
+                            {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                        </div>
+
+                        <input className='btn btn-outline w-full my-3 text-white' value="Login" type="submit" />
+                        <div>
+                            {loginError && <p className='text-red-600'>{loginError}</p>}
+                        </div>
+                    </form>
+
+                    <p className='text-white'>
+                        Don't have an account? <Link className='text-secondary' to="/signup">Sign Up Here</Link>
+                    </p>
+
+                    <div className="divider text-white">OR</div>
+
+                    <button className='btn btn-outline w-full text-white'>CONTINUE WITH GOOGLE</button>
                 </div>
             </div>
         </Fragment>
--- src/Pages/User/SignUp/SignUp.js
+++ src/Pages/User/SignUp/SignUp.js
@@ -1,48 +1,83 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-const SignUp = () => {
+const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [loginError, setLoginError] = useState('');
+
+    const handleSignUp = (data) => {
+        console.log(data);
+    }
+
     return (
         <Fragment>
-            <div className="hero flex h-screen justify-center items-center bg-black">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="card flex-shrink-0 shadow-2xl bg-white">
-                        <h1 className="text-center text-black text-3xl font-bold mt-3">Sign Up</h1>
-                        <div className="card-body">
+            <div className='flex justify-center items-center bg-black'>
+                <div className='border border-white px-20 py-8 mb-3'>
+                    <h2 className='text-5xl text-white mb-6 text-center'>Sign Up</h2>
+
+                    <form onSubmit={handleSubmit(handleSignUp)}>
+                        <div className="form-control w-full">
+                            <input placeholder='Full Name' type="text"
+                                {...register("name", {
+                                    required: "Your Full Name is Required"
+                                })}
+                                className="input input-bordered w-full my-3" />
+                            {errors.name && <p className='text-red-600'>{errors.name?.message}</p>}
+                        </div>
+
+                        <div className="form-control w-full">
+                            <input placeholder='Email' type="email"
+                                {...register("email", {
+                                    required: "Your Email Address is Required"
+                                })}
+                                className="input input-bordered w-full my-3" />
+                            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                        </div>
+
+                        <div className="form-control w-full">
+                            <input placeholder='******' type="password"
+                                {...register("password", {
+                                    required: "Your Password is Required",
+                                    minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                                })}
+                                className="input input-bordered w-full my-3" />
+                            {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                        </div>
+
+                        <div className='border border-white rounded-lg my-3 px-3'>
                             <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
+                                <label className="label cursor-pointer">
+                                    <span className="label-text text-white">Buyer</span>
+                                    <input {...register("Role", { required: true })} className="radio" type="radio" value="buyer" checked />
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
                             </div>
-
                             <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
+                                <label className="label cursor-pointer">
+                                    <span className="label-text text-white">Seller</span>
+                                    <input {...register("Role", { required: true })} className="radio" type="radio" value="seller" />
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label mt-2">
-                                    <Link to='/login' className="label-text-alt link link-hover">
-                                        Already Have an Account? Login Here
-                                    </Link>
-                                </label>
-                            </div>
-
-                            <div className="form-control">
-                                <button className="btn btn-primary">Sign Up</button>
-                            </div>
-
-                            <div className="divider text-black">OR</div>
-
-                            <div className="form-control">
-                                <button className="btn btn-danger">Google</button>
                             </div>
                         </div>
-                    </div>
+
+                        <input className='btn btn-outline w-full my-3 text-white' value="Sign Up" type="submit" />
+                        <div>
+                            {loginError && <p className='text-red-600'>{loginError}</p>}
+                        </div>
+                    </form>
+
+                    <p className='text-white'>
+                        Already have an account? <Link className='text-secondary' to="/login">Login Here</Link>
+                    </p>
+
+                    <div className="divider text-white">OR</div>
+
+                    <button className='btn btn-outline w-full text-white'>CONTINUE WITH GOOGLE</button>
                 </div>
             </div>
         </Fragment>
     );
 };
 
-export default SignUp;
+export default Login;
